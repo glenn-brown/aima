5aaf71af0fe75a2c335bb4562f3f1b7ef9964e0d
Glenn Brown <gbrown@alumni.caltech.edu>
2012-10-19 00:03:19 -0700

    [romania] Add ExampleNew()

--- ch3/romania/romania_test.go
+++ ch3/romania/romania_test.go
@@ -1,9 +1,15 @@
 package romania
 
 import (
+	"fmt"
 	"github.com/glenn-brown/aima/ch3/graph"
 	"testing"
 )
+
+func ExampleNew() {
+	fmt.Println (graph.RecursiveBestFirstSearch(New(Arad)))
+	// Output: [Sibiu RimnicuVilcea Pitesti Bucharest]
+}
 
 func BenchmarkAradRBFS(b *testing.B) {
 	for i := b.N; i >= 0; i-- {
